626b2bab2bb8c427a0ebe717436cabd4051bdf17
Fanny Eber <fannyeber@gmail.com>
2022-09-22 17:16:14 +0200

    form finish

--- TP2/src/app/app.module.ts
+++ TP2/src/app/app.module.ts
@@ -7,11 +7,18 @@ import { FooterComponent } from './footer/footer.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
 import { ClientInfoComponent } from './client-info/client-info.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 const routes: Routes = [
-  { path: '', component: FormComponent }
+  { path: '', component: FormComponent },
+  { path: 'client-info', component: ClientInfoComponent },
 ];
-
 
 @NgModule({
   declarations: [
@@ -23,7 +30,14 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
